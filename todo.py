81a4dd5909f46112226c0010cce2e371d6174f43
Tense YT <>
2021-04-26 15:23:18 +0000

    shoow

--- todo.py
+++ todo.py
@@ -1,18 +1,27 @@
 HELP = """
+help - список команд
+add - добавить событие
+show - показать элемент
+remove - удалить элемент
+exit - закрыть программу
 help - помощь
 add  - добавить
 show  - показать
-done   - убрать
+done   - убрать	
 exit    - выкл
-"""
-
+	  """
+ 
+todo = {}
+ # 1 - запрос даты
 todo = {} 
 print("Введите команду или help, для вывода доступных команд")
+# 2 - запрос задачи
+# 0 - none
 
 while True:
-  userUnswer = input()
+  userAnswer = input()
 
-  if userUnswer == "add":
+  if userAnswer == "add":
     userDate = input("Введите дату:\n")
     userTask = input("Что нужно сделать?")
 
@@ -22,11 +31,13 @@ while True:
       todo[ userDate ] = ( userTask )
     todo[ userDate] = userTask
     print(f" [ {userDate} ] - добавленная задача '{userTask}' ")
-  elif userUnswer == "help":
+  elif userAnswer == "help":
     print(HELP) 
-  elif userUnswer == "show":
-    for  tasks in todo [ date]
-  elif userUnswer == "exit":
+  elif userAnswer == "show":
+		for date in sorted( todo.keys() ):
+      for  tasks in todo [ date]:
+			  print(f"[ { date } ] - {tasks}")
+  elif userAnswer == "exit":
     break 
-  elif userUnswer == "help":
+  elif userAnswer == "help":
     print("Работает") 
